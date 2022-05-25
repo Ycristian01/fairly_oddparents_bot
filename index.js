59e4fb959c7d25db40874ded243f88e27e6727c0
Cristian <ycristian@uninorte.com>
2022-05-25 16:07:45 -0500

    created bot instance with interval (every 3 hours)

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const {TwitterClient} = require('twitter-api-client')
 const axios = require('axios')
 
+// Create object reference to libs
+var Twit = require('twit');
+var fs = require('fs');
+
 const twitterClient = new TwitterClient({
 	apiKey: process.env.TWITTER_API_KEY,
 	apiSecret: process.env.TWITTER_API_SECRET,
@@ -9,7 +13,19 @@ const twitterClient = new TwitterClient({
 	accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-axios.get('http://localhost:3000/tweets')
+// Set interval time. Try to use a not so small interval to avoid Twitter to lock your account.
+var INTERVAL = 3*60*60*1000; // 3 hours
+
+function BotStart() {
+	console.log("> Twitter bot is running (" + Date() + ")...")
+
+	PostTweet()
+}
+
+
+function PostTweet() {
+
+	axios.get('http://localhost:3000/tweets')
     .then(response => {
     const data = response.data ? response.data : {}
 		// console.log(data[0])
@@ -31,7 +47,13 @@ axios.get('http://localhost:3000/tweets')
 	}).catch(err => {
 		console.error(err)
 	})
-}).catch (err => {
+	}).catch (err => {
     console.error(err)
-})
+	})
+}
+
+// Start bot and timer
+BotStart();
+setInterval(BotStart, INTERVAL);
+
 
